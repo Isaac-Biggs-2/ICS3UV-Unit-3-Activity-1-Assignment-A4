1bfadea0ab76e00ea10810e19a7d44db53d4978a
default <isaac.biggs@stu.ocsb.ca>
2025-11-24 22:34:24 +0000

    Program to calculate average of three marks

---
+++ main.ts
@@ -0,0 +1,19 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date 2025-11-24
+ * @fileoverview This program will calculate the average of three marks
+*/
+
+//variables
+let mark1: number = (56.9)
+let mark2: number = (89.7)
+let mark3: number = (90.2)
+
+//data variable for average
+let average = ((mark1 + mark2 + mark3)/3)
+
+//display average grade to screen
+console.log("The average of 56.9, 89.7, and 90.2 is " + average)
+
+console.log("\nDone.")
